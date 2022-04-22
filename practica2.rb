19a7d23d00ad23379582939840da197a2deff8a3
Rojego <rjerez@gmail.com>
2022-04-21 22:30:20 -0400

    Commit First

---
+++ practica2.rb
@@ -0,0 +1,24 @@
+#3.times {puts "Hola Mundo"}#
+
+#=begin#
+#print "cadena"#
+#print variable, (el print no agrega un salio de linea)#
+
+
+
+#=end#
+#puts "ingresa un numero"#
+#x = gets.chomp#
+#puts "ingresaste el numero : "#
+#puts "ingresa tu nombre"#
+#nombre = gets.chomp#
+#puts "hola #{nombre} , como estas"#
+
+#nombre = ARGV[0]#
+#nombre2 = ARGV[1]#
+#puts "Hola #{nombre} y #{nombre2} , como estas ?"#
+
+# num1 = gets.chomp.to_i # 4 si  le quitamos chomp tiene el mismo resultado
+# num2 = gets.chomp.to_i # 6
+# puts num1 + num2 # 46# 
+# puts num1 + num2 #46
